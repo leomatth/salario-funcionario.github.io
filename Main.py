dbbf976d14d60da3f4530de112550436d3b2d919
leomatth <leomattheus95@gmail.com>
2024-01-26 22:09:47 -0300

    First commit

---
+++ Main.py
@@ -0,0 +1,14 @@
+class Main:
+    pass
+
+from Cliente import Cliente
+
+from Conta import Conta
+
+
+c1 = Cliente("João", "114444-2222")
+conta = Conta(c1.get_nome(), 1222)
+
+conta.deposita(100)
+conta.saque(50)
+conta.extrato()
